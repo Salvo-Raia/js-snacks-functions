c8a1f4ace7937fa997dc3d84694aafec368909e0
Salvo-Raia <salvo.raia@outlook.it>
2026-01-09 22:38:38 +0100

    Add function to snack 03

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,10 +1,18 @@
 /* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
-
+const selectLetter = prompt("Scrivi un'iniziale", "M")
 
 // Dichiara la funzione qui.
-
+function FirstDigitFilter (names) {
+    let filteredNames = []
+    for (i = 0; i < names.length; i++) {
+    let currentName = names; 
+    if (currentName[i].charAt(0) == selectLetter) {
+        filteredNames.push(names[i])
+    }
+} return filteredNames
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
