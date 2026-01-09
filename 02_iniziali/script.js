622dacf385f614c3cb9e11bc324f84237b5e07fa
Salvo-Raia <salvo.raia@outlook.it>
2026-01-09 22:22:24 +0100

    Add function to snack 02

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,9 +1,14 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca un nuovo array con le iniziali di ogni parola dell'array fornito */
-
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
-
+function onlyFirstDigit (names) {
+    firstDigitArray = []; 
+    for (i = 0; i < names.length; i++) {
+        firstDigitArray.push (names[i].charAt(0))
+    }
+    return firstDigitArray;    
+} 
 // Invoca la funzione qui e stampa il risultato in console
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
